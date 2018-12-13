3b6e75da0b14de882833479957ff90e8213198a2
mohammad <khezri@outlook.com>
2018-12-13 14:01:24 -0600

    Applied dark theme

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -12,8 +12,12 @@ class Auth extends React.Component {
 
   render() {
     return (
-      <div className="Auth">
-        <button className="btn btn-success" onClick={this.authenticateUser}>Login with your GitHub account</button>
+      <div className="auth-container d-flex justify-content-center bg-dark">
+        <div className="Auth m-5 p-5 text-center border border-secondary w-50 rounded bg-dark shadow-sm">
+          <h1 className="display-2 text-info">Developer Portal</h1>
+          <p className="text-light">Please login with your GitHub account</p>
+          <button className="btn btn-success btn-lg w-25" onClick={this.authenticateUser}>Login</button>
+        </div>
       </div>
     );
   }
