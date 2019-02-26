232c6d3d721b0e330c88312c57f8377d1fa892e9
mohammad <khezri@outlook.com>
2019-02-25 19:07:39 -0600

    fed up with API git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,33 +1,17 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './Profile.scss';
 
 class Profile extends React.Component {
-  static propTypes = {
-    profile: PropTypes.object,
-    commits: PropTypes.number,
-  }
-
   render() {
-    const { profile, commits } = this.props;
+
     return (
       <div className="profile col">
-        <div className="avatar">
-          <img className="pic" id="me" src={profile.avatar_url} alt="avatar" />
-          <div>
-            <h3 className="name">{profile.name}></h3>
-            <p className="login">{profile.login}></p>
-            <p className="bio">{profile.bio}></p>
-            <a href={profile.html_url} className="_blank profile-link-text">{profile.html_url}></a>
-            <div className="text-center mt-2">
-              <h3>{commits}</h3>
-              <p className="commits-text">commits in the last 5 days</p>
-            </div>
-          </div>
-        </div>
+        <h2>Profile</h2>
+
       </div>
     );
   }
 }
+
 
 export default Profile;
