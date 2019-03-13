da55eee579ba39706bcbf50cffb2d13292a076f5
mohammad <khezri@outlook.com>
2019-03-12 19:06:31 -0500

    printing profile

--- src/App/App.js
+++ src/App/App.js
@@ -18,62 +18,71 @@ class App extends Component {
   }
 
   componentDidMount() {
-    connection();
-    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-        this.setState({
-          authed: true,
-        });
-      } else {
-        this.setState({
-          authed: false,
-        });
-      }
-    });
+    // Here we get the API data and test by console log to make sure the data is returned. 
+    getUser(user) // I used my gitHub username as a test to
+      .then((result) => {
+        console.log(result);
+        this.setState({ profile: result });
+      })
+      .catch(err => console.log(err));
   }
 
-  componentWillUnmount() {
-    this.removeListener();
-  }
-
-  isAuthenticated = () => {
-    this.setState({ authed: true });
-  }
-
-  render() {
-    const logoutClickEvent = () => {
-      authRequest.logoutUser();
-      this.setState({ authed: false });
-    };
-
-    if (!this.state.authed) {
-      return (
-        <div className="App">
-          <MyNavbar isAuthed={this.state.authed} logoutClickEvent={logoutClickEvent} />
-          <Auth isAthenticated={this.isAuthenticated} />
-        </div >
-      );
+  connection();
+  this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      this.setState({
+        authed: true,
+      });
+    } else {
+      this.setState({
+        authed: false,
+      });
     }
+  });
+}
 
+componentWillUnmount() {
+  this.removeListener();
+}
+
+isAuthenticated = () => {
+  this.setState({ authed: true });
+}
+
+render() {
+  const logoutClickEvent = () => {
+    authRequest.logoutUser();
+    this.setState({ authed: false });
+  };
+
+  if (!this.state.authed) {
     return (
       <div className="App">
         <MyNavbar isAuthed={this.state.authed} logoutClickEvent={logoutClickEvent} />
-        <div className="container rounded">
-          <div className="row">
-            <Profile profile='Mohammad' avatar='avatar' />
-            <div className="col-sm-9">
-              <Input />
-              <Output />
-            </div>
-
-          </div>
-          <div className="row my-3">
-            <Chart />
-          </div>
-        </div>
-      </div>
+        <Auth isAthenticated={this.isAuthenticated} />
+      </div >
     );
   }
+
+  return (
+    <div className="App">
+      <MyNavbar isAuthed={this.state.authed} logoutClickEvent={logoutClickEvent} />
+      <div className="container rounded">
+        <div className="row">
+          <Profile profile='Mohammad' avatar='avatar' />
+          <div className="col-sm-9">
+            <Input />
+            <Output />
+          </div>
+
+        </div>
+        <div className="row my-3">
+          <Chart />
+        </div>
+      </div>
+    </div>
+  );
+}
 }
 
 export default App;
--- src/helpers/data/GithubRequests.js
+++ src/helpers/data/GithubRequests.js
@@ -1,32 +1,17 @@
 import axios from 'axios';
-import apiKeys from '../apiKeys';
+import apiKeys from '../data/apiKeys';
 
-const githubUrl = apiKeys.githubApi.apiUrl;
+const clientId = apiKeys.githubApi.client_id; // The client_id is exactly the key in apiKeys in gitHub. 
+const clientSecret = apiKeys.githubApi.client_secret; //The client_secret is exactly the key in apiKeys in gitHub.
 
-const getUserInfo = userName => new Promise((resolve, reject) => {
-  axios.get(`${githubUrl}/users/${userName}`)
+const getUser = gitHubUserName => new Promise((resolve, reject) => {
+  axios
+    .get(`https://api.github.com/users/${gitHubUserName}?client_id=${clientId}&client_secret=${clientSecret}`)
     .then((result) => {
+      console.log(result);
       resolve(result.data);
     })
     .catch(err => reject(err));
 });
 
-const getCommits = userName => new Promise((resolve, reject) => {
-  axios.get(`${githubUrl}/users/${userName}/events/public`)
-    .then((results) => {
-      let commitCounter = 0;
-      const commits = results.data.filter(event => event.type === 'PushEvent');
-      commits.forEach((commit) => {
-        commitCounter += commit.payload.commits.length;
-        // console.log(commit.payload.commits);
-      });
-      resolve(commitCounter);
-    })
-    .catch(err => reject(err));
-});
-
-
-export default {
-  getUserInfo,
-  getCommits,
-};
+export default getUser;
