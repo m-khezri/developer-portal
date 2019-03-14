50a550b5377e3172908c09d724e3f2ecdf3fc720
mohammad <khezri@outlook.com>
2019-03-13 23:40:43 -0500

    finally printed bio and image! :(

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import connection from '../helpers/data/connection';
 import Auth from '../components/Auth/Auth';
 import './App.scss';
+import getUser from '../helpers/data/GithubRequests';
 import MyNavbar from '../components/MyNavbar/myNavbar';
 import Profile from '../components/Profile/Profile';
 import Chart from '../components/chart/chart';
@@ -12,77 +13,72 @@ import firebase from 'firebase/app';
 
 class App extends Component {
   state = {
-    authed: false,
-    profile: '',
-    avatar: '',
+    profile: {},
   }
 
   componentDidMount() {
-    // Here we get the API data and test by console log to make sure the data is returned. 
-    getUser(user) // I used my gitHub username as a test to
+    getUser('m-khezri') // I used my gitHub username as a test to
       .then((result) => {
         console.log(result);
         this.setState({ profile: result });
       })
       .catch(err => console.log(err));
+
+    connection();
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({
+          authed: true,
+        });
+      } else {
+        this.setState({
+          authed: false,
+        });
+      }
+    });
   }
 
-  connection();
-  this.removeListener = firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-      this.setState({
-        authed: true,
-      });
-    } else {
-      this.setState({
-        authed: false,
-      });
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  isAuthenticated = () => {
+    this.setState({ authed: true });
+  }
+
+  render() {
+    const logoutClickEvent = () => {
+      authRequest.logoutUser();
+      this.setState({ authed: false });
+    };
+
+    if (!this.state.authed) {
+      return (
+        <div className="App">
+          <MyNavbar isAuthed={this.state.authed} logoutClickEvent={logoutClickEvent} />
+          <Auth isAthenticated={this.isAuthenticated} />
+        </div >
+      );
     }
-  });
-}
 
-componentWillUnmount() {
-  this.removeListener();
-}
-
-isAuthenticated = () => {
-  this.setState({ authed: true });
-}
-
-render() {
-  const logoutClickEvent = () => {
-    authRequest.logoutUser();
-    this.setState({ authed: false });
-  };
-
-  if (!this.state.authed) {
     return (
       <div className="App">
         <MyNavbar isAuthed={this.state.authed} logoutClickEvent={logoutClickEvent} />
-        <Auth isAthenticated={this.isAuthenticated} />
-      </div >
-    );
-  }
-
-  return (
-    <div className="App">
-      <MyNavbar isAuthed={this.state.authed} logoutClickEvent={logoutClickEvent} />
-      <div className="container rounded">
-        <div className="row">
-          <Profile profile='Mohammad' avatar='avatar' />
-          <div className="col-sm-9">
-            <Input />
-            <Output />
+        <div className="container rounded">
+          <div className="row">
+            <Profile profile={this.state.profile} />
+            <div className="col-sm-9">
+              <Input />
+              <Output />
+            </div>
           </div>
-
-        </div>
-        <div className="row my-3">
-          <Chart />
+          <div className="row my-3">
+            <Chart />
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 }
 
 export default App;
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,8 +3,10 @@ import React from 'react';
 class Profile extends React.Component {
   render() {
     return (
-      <div>
-        <h1>{this.props.profile}</h1>
+      <div className="bg-white rounded shadow-sm p-3 w-25">
+        <h4 className="text-center">{this.props.profile.name}</h4>
+        <p className="text-center">{this.props.profile.bio}</p>
+        <img className="w-75 rounded-circle mx-auto d-block" src={this.props.profile.avatar_url} alt="user profile" />
       </div>
     );
   }
