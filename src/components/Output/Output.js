88dafcae398eb7324ac4b971e2b3bb5cd30bc2b8
mohammad <khezri@outlook.com>
2019-04-10 17:14:32 -0500

    forms

--- src/components/Output/Output.js
+++ src/components/Output/Output.js
@@ -4,36 +4,13 @@ import './Output.scss';
 class Output extends React.Component {
   render() {
     return (
-      <div className="Output bg-white rounded shadow-sm my-3">
-        <ul className="nav nav-tabs">
-          <li className="nav-item">
-            <a className="nav-link" data-toggle="tab" href="#Toturials-tab">Toturials</a>
-            <p>Name</p>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" data-toggle="tab" href="#Blog-tab">Blog</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" data-toggle="tab" href="#Resources-tab">Resources</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" data-toggle="tab" href="#Podcast-tab">Podcast</a>
-          </li>
+      <div className="card mt-3">
+        <ul>Tutorial
+          <li></li>
         </ul>
-
-        <div class="tab-content">
-          <div className="tab-pane container active" id="Toturials-tab">
-          </div>
-          <div className="tab-pane container fade" id="Blog-tab">
-          </div>
-          <div className="tab-pane container fade" id="Resources-tab">
-          </div>
-          <div className="tab-pane container fade" id="Podcast-tab">
-          </div>
-        </div>
-
-
       </div>
+
+
     );
   }
 }
